272d573e265dd295503c681710b207b4f00bb393
Valeriy Avanesov <acopich@gmail.com>
2021-03-16 13:48:46 +0100

    Training is faster now.

--- src/main/scala/de/wias/nonparregboot/classifier/ClassifierApp.scala
+++ src/main/scala/de/wias/nonparregboot/classifier/ClassifierApp.scala
@@ -4,7 +4,6 @@ import breeze.optimize.{FirstOrderMinimizer, LBFGS}
 import cats._
 import cats.data._
 import cats.implicits._
-import cats.effect.{ExitCode, IO, IOApp}
 import com.github.fommil.netlib.BLAS
 import de.wias.tfrandom.{Random, sampleMean}
 import org.platanios.tensorflow.api.core.Shape
@@ -14,14 +13,23 @@ import scalapurerandom.SeqReducibleInstance._
 import org.log4s
 import scribe.Logger
 import scribe.Level
+import cats.effect.{IO, _}
+import org.platanios.tensorflow.api.tf
+
+import scala.concurrent.duration.MILLISECONDS
 
 object ClassifierApp extends IOApp with AveragebleHelper with NatHelperTrait with HasSizeHelper {
-  def printIO(s: Any) = IO(println(s))
+  def timeAndPrint(s: => Any)(implicit clock: Clock[IO]) = for {
+    start  <- clock.monotonic(MILLISECONDS)
+    _      <- IO(println(s))
+    finish <- clock.monotonic(MILLISECONDS)
+    _      <- IO(println(f"Completed in ${(finish - start).toFloat / 1000}s"))
+  } yield ()
 
   override def run(args: List[String]): IO[ExitCode] = {
     Logger.root.clearHandlers()
                 .clearModifiers()
-                .withHandler(minimumLevel = Some(Level.Info))
+                .withHandler(minimumLevel = Some(Level.Debug))
                 .replace()
 
     val metric = accuracy + entropy
@@ -29,9 +37,10 @@ object ClassifierApp extends IOApp with AveragebleHelper with NatHelperTrait wit
     val kernel: Kernel = Matern72(1f)
     val lambda = 1f
 
-    val classifierTrainer = TFKRC.krc(lambda, kernel, 1e-5f)
+    val optimizer = tf.train.Adam(0.01f)
+    val classifierTrainer = TFKRC.krc(lambda, kernel, optimizer, 1e-5f)
 
-    val n = 123
+    val n = 1234
     val testSize = 30
     val m = 4
     val result: Random[MetricValue] = for {
@@ -46,6 +55,6 @@ object ClassifierApp extends IOApp with AveragebleHelper with NatHelperTrait wit
 
     val iters = p"10"
     val avgMetricValue: Random[MetricValue] = sampleMean(result, iters)
-    printIO(avgMetricValue.sample(13)).as(ExitCode.Success)
+    timeAndPrint(avgMetricValue.sample(13)).as(ExitCode.Success)
   }
 }
--- src/main/scala/de/wias/nonparregboot/classifier/TFKRC.scala
+++ src/main/scala/de/wias/nonparregboot/classifier/TFKRC.scala
@@ -1,23 +1,24 @@
 package de.wias.nonparregboot.classifier
 
 import breeze.linalg.DenseMatrix
-import org.platanios.tensorflow.api.{Graph, Session, Shape, Tensor, tf}
+import org.platanios.tensorflow.api.{Graph, Output, Session, Shape, Tensor, tf}
 import scalapurerandom.{DV, toNEV}
 import cats._
 import cats.data._
 import cats.implicits._
 import org.log4s.Logger
+import org.platanios.tensorflow.api.ops.training.optimizers.Optimizer
 import scribe.Logging
 
 
 object TFKRC  extends Logging {
 
-  def getLoss(K: OFloat, Y: Classes, alpha: VF, lambda: Float) = {
+  def getLoss(K: OFloat, Ywithrange: Output[Int], alpha: VF, lambda: Float) = {
     import org.platanios.tensorflow.api._
 
     val Kalpha = tf.matmul(K, alpha) // n * m
     val expKalphak = Kalpha.exp
-    val termI = - Y.toArray.zipWithIndex.map { case(yi, i) => Kalpha(i, yi) }.reduce(_ + _)
+    val termI = - Kalpha.gatherND(Ywithrange).sum()
     val termII = tf.sum(expKalphak, 1).log.sum()
     val termIII = 0.5f * lambda * tf.sum(tf.multiply(alpha, Kalpha))
 
@@ -27,18 +28,23 @@ object TFKRC  extends Logging {
 
   def krc(lambda: Float,
           kernel: Kernel,
+          optimizer: Optimizer,
           tol: Float): ClassifierTrainer = (X: Covariates, Y: Classes, init: Init) => {
     val K = kernel(X, X)
 
     val alpha = tf.variable[Float]("alpha", init.shape, initializer = tf.ConstantInitializer(init))
 
-    val lossOp = getLoss(K, Y, alpha, lambda)
-    val trainOp = tf.train.Adam(0.01f).minimize(lossOp)
+    val n = alpha.shape(0)
+    val index = Tensor(0 until n).reshape(-1)
+    val doubleIndex = tf.stack(List[Output[Int]](index, Y), axis=1)
+
+    val lossOp = getLoss(K, doubleIndex, alpha, lambda)
+    val trainOp = optimizer.minimize(lossOp)
 
     val session = Session()
     session.run(targets = tf.globalVariablesInitializer())
 
-    val iters = LazyList.from(0)
+    val iters = LazyList.from(1)
     val losses =  iters.map { iter =>
       val lossT = session.run(targets = trainOp, fetches = lossOp)
       val loss = lossT.scalar
@@ -46,7 +52,7 @@ object TFKRC  extends Logging {
       loss
     }
 
-    val ((totalIters, _), loss) = iters.zip(losses).zip(losses.tail).takeWhile { case((iter, prevLoss), loss) =>
+    val ((totalIters, _), loss) = iters.zip(losses).zip(losses.tail).takeWhile { case((_, prevLoss), loss) =>
       math.abs(prevLoss - loss) / prevLoss > tol
     }.last
 
